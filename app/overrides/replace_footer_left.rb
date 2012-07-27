83142fe69eef73eb6e9af4b8f6664730b5897bc5
Chris Whitman <chris@theoryplus.com>
2012-07-27 08:27:52 -0700

    Footer deploy fix

--- app/overrides/replace_footer_left.rb
+++ app/overrides/replace_footer_left.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application", 
                      :name => "replace_footer_left", 
                      :replace_contents => "#footer-left",
-                     :text => "<%= render_snippet 'footer_left' %>")
+                     :text => "<%= link_to 'Sisbro.com Home', 'http://sisbro.com' %>")
