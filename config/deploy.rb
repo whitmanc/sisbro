4079d9b6569be748a34c91174c063dd5ea04b372
Chris Whitman <chris@theoryplus.com>
2012-07-27 15:11:40 -0700

    More WIP: Capistrano retain images on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,29 +50,21 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/config/Procfile #{release_path}/Procfile"
     run "ln -nfs #{shared_path}/config/.foreman #{release_path}/.foreman"
   end
-  
-  task :ln_assets do
-    run <<-CMD
-      rm -rf #{latest_release}/public/assets &&
-      mkdir -p #{shared_path}/assets &&
-      ln -s #{shared_path}/assets #{latest_release}/public/assets
-    CMD
-  end
-  
-  task :assets do
-    update_code
-    ln_assets
-  
-    run_locally "rake assets:precompile"
-    run_locally "cd public; tar -zcvf assets.tar.gz assets"
-    top.upload "public/assets.tar.gz", "#{shared_path}", :via => :scp
-    run "cd #{shared_path}; tar -zxvf assets.tar.gz"
-    run_locally "rm public/assets.tar.gz"
-    run_locally "rm -rf public/assets"
-  
-    create_symlink
-    restart
-  end
+
+  #
+  # TODO: Implement this performance boost! assets:precompile is s.l.o.w.
+  #
+  # namespace :assets do
+  #   desc "Precompile assets only when necessary. Hello Boost!"
+  #   task :precompile, :roles => :web, :except => { :no_release => true } do
+  #     from = source.next_revision(current_revision)
+  #     if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+  #       run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+  #     else
+  #       logger.info "Skipping asset pre-compilation because there were no asset changes"
+  #     end
+  #   end
+  # end
 end
 
 namespace :db do
@@ -89,4 +81,3 @@ after 'deploy:start', 'foreman:start'
 
 before 'deploy:restart', 'foreman:export'
 after 'deploy:restart', 'foreman:restart'
-
