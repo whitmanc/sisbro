5cf42252a8ca3f867f1c0835d13800b95634aee4
Chris Whitman <chris@theoryplus.com>
2012-07-27 14:53:51 -0700

    Remove spree_editor's JS assets

--- app/assets/javascripts/admin/all.js
+++ app/assets/javascripts/admin/all.js
@@ -10,5 +10,4 @@
 //= require admin/spree_promo
 
 //= require_tree .
-//= require admin/spree_editor
 //= require admin/spree_product_sort
--- app/models/image_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::Image.class_eval do
-  attachment_definitions[:attachment][:styles] = {
-    :mini => '48x48>', # thumbs under image
-    :small => '100x100>', # images on category view
-    :product => '190x190>', # full product image
-    :medium => '240x240>', # full product image
-    :large => '600x600>'  # light box image
-  }
-end
