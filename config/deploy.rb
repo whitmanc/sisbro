becd5c105589edbfff9f61a5c1b3fc33c6ffa1e2
Chris Whitman <chris@theoryplus.com>
2012-07-27 08:52:44 -0700

    More performance boost related

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,17 +51,18 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/config/.foreman #{release_path}/.foreman"
   end
   
-  namespace :assets do
-     task :precompile, :roles => :web, :except => { :no_release => true } do
-       from = source.next_revision(current_revision)
-       if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-       else
-         logger.info "Skipping asset pre-compilation because there were no asset changes"
-       end
-     end
-   end
+namespace :assets do
+  desc "Precompile assets only when necessary. Hello Boost!"
+  task :precompile, :roles => :web, :except => { :no_release => true } do
+    from = source.next_revision(current_revision)
+    if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+    else
+      logger.info "Skipping asset pre-compilation because there were no asset changes"
+    end
+  end
 end
+  
  
 end
 namespace :db do
