a75b867af2e26f454698384989d0e65dab99a5c6
Chris Whitman <chris@theoryplus.com>
2012-07-27 15:54:58 -0700

    Yet more WIP: Capistrano retain images on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,6 +44,17 @@ namespace :foreman do
 end
 
 namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+  
   desc "Symlink shared configs and folders on each release."
   task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
