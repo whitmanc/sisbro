a7276e5a36337b692ddfd498ed414b50293850d2
Chris Whitman <chris@theoryplus.com>
2012-07-27 13:30:33 -0700

    Remove spree_editor ext

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,21 +54,6 @@ namespace :deploy do
   task :precompile, :role => :app do  
     run "cd #{release_path}/ && rake assets:precompile"  
   end  
-  
-  #
-  # TODO: Implement this performance boost! assets:precompile is s.l.o.w.
-  #
-  # namespace :assets do
-  #   desc "Precompile assets only when necessary. Hello Boost!"
-  #   task :precompile, :roles => :web, :except => { :no_release => true } do
-  #     from = source.next_revision(current_revision)
-  #     if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-  #       run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-  #     else
-  #       logger.info "Skipping asset pre-compilation because there were no asset changes"
-  #     end
-  #   end
-  # end
 end
 
 namespace :db do
