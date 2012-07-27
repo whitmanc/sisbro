3c421d5c5dbdbac65f3ee70c54257782d5b76301
Chris Whitman <chris@theoryplus.com>
2012-07-27 04:20:21 -0700

    WIP: Sisbro custom calculators

--- db/migrate/20120721084535_namespace_faq_tables.rb
+++
@@ -1,6 +0,0 @@
-class NamespaceFaqTables < ActiveRecord::Migration
-  def change
-    rename_table :questions, :spree_questions
-    rename_table :question_categories, :spree_question_categories
-  end
-end
