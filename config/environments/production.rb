1debc3d9cf46c82ce3c12b95da98f49bf8f8fe7a
Chris Whitman <chris@theoryplus.com>
2012-07-27 17:43:07 -0700

    Yet more WIP: Capistrano retain images on deploy, use asset_path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Sisbro::Application.configure do
+    config.deface.enabled = true
   ## Code is not reloaded between requests
     config.cache_classes = true
 
@@ -54,7 +55,6 @@ Sisbro::Application.configure do
     # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
     # the I18n.default_locale when a translation can not be found)
     config.i18n.fallbacks = true
-   config.deface.enabled = true
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
