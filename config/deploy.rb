764a297e8ca720062478cf637e26d5d6cd4ea7e0
Chris Whitman <chris@theoryplus.com>
2012-07-27 17:37:01 -0700

    Yet more WIP: Capistrano retain images on deploy, use asset_path

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ namespace :foreman do
       task :precompile, :roles => :web, :except => { :no_release => true } do
         from = source.next_revision(current_revision)
         if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile --trace}
         else
           logger.info "Skipping asset pre-compilation because there were no asset changes"
         end
