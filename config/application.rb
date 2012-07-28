f78837a2c4d3129f272aa2bd72c8fcc7e65dce9f
Chris Whitman <chris@theoryplus.com>
2012-07-27 17:49:49 -0700

    Yet more WIP: Capistrano retain images on deploy, use asset_path

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-#require 'rails/all'
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "rails/test_unit/railtie"
-require "sprockets/railtie"
-
-
+require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test production))
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
@@ -72,10 +65,9 @@ module Sisbro
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    
-    config.assets.initialize_on_precompile = false
-
+    # Enable the asset pipeline
     config.assets.enabled = true
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
