697481621d3184560342a478c2172bdd1822f880
Chris Whitman <chris@theoryplus.com>
2012-07-29 09:26:50 -0700

    Dont precompile assets if unecessary

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,6 +54,17 @@ namespace :deploy do
   task :precompile, :role => :app do  
     run "cd #{release_path}/ && rake assets:precompile"  
   end  
+  
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
 end
 
 before 'deploy:assets:precompile', 'deploy:symlink_shared'
