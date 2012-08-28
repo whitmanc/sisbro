3870a0571467393deb6f46b99f140aeddd4b4af7
Chris Whitman <chris@theoryplus.com>
2012-08-28 09:51:48 -0700

    Rearrange homepage products

--- app/assets/javascripts/store/homepage.js
+++ app/assets/javascripts/store/homepage.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('#product_21').detach().prependTo('#products');
   $('#product_26').remove();
-  $('#product_27').remove();
-  $('#product_21').detach().insertAfter('#product_2');
+  $('#product_19').remove();
+  $('#product_4').detach().insertAfter('#product_2');
 });
