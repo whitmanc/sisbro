04bc4d103d731000a72f638fb763a06f8095ab84
Chris Whitman <chris@theoryplus.com>
2012-08-28 09:37:13 -0700

    Reposition product on homepage

--- app/assets/javascripts/store/homepage.js
+++ app/assets/javascripts/store/homepage.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
   $('#product_21').detach().prependTo('#products');
   $('#product_26').remove();
   $('#product_27').remove();
+  $('#product_21').detach().insertAfter('#product_2');
 });
